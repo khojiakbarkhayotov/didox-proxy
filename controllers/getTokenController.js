2270e150d4c89d6ca11b1e1d00ac05e5c43c6347
khayotovkhojiakbar <khayotovkhojiakbar@gmail.com>
2025-01-22 15:29:27 +0500

    added new data

--- controllers/getTokenController.js
+++ controllers/getTokenController.js
@@ -1,4 +1,4 @@
-import attachSignature from '../services/attachSignature.js';
+import attachSignature from "../services/attachSignature.js";
 
 const getTokenController = async (req, res) => {
   try {
@@ -9,14 +9,14 @@ const getTokenController = async (req, res) => {
     }
 
     try {
-      res.status(200).json({ sign });
+      res.status(200).json(sign);
     } catch (parseError) {
       console.error("Failed to parse sign as JSON:", parseError.message);
       res.status(400).json({ error: sign });
     }
   } catch (error) {
     console.error("Error in getTokenController:", error.message);
-    res.status(500).json({ error: error.message }); 
+    res.status(500).json({ error: error.message });
   }
 };
 
--- services/attachSignature.js
+++ services/attachSignature.js
@@ -1,40 +1,36 @@
 import dotenv from "dotenv";
 import getSignature from "./createSignature.js";
-import axios from "axios";  // Add axios for making HTTP requests
+import axios from "axios";
+import createWebSocketConnection from "./socket.js";
 dotenv.config();
 
 const attachSignature = async (req) => {
   try {
-    // Step 1: Get the signature
     const response = await getSignature(req);
 
-    // Step 2: Prepare the payload for the POST request
     const message = {
-      pkcs7: response.key, // Value of the pkcs7_64 field
-      signatureHex: response.signature, // Value of the signature_hex field
+      pkcs7: response.key,
+      signatureHex: response.signature,
     };
 
-    // Step 3: Send the POST request
     const postResponse = await axios.post(
       "https://stage.goodsign.biz/v1/dsvs/timestamp",
       message,
       {
         headers: {
-          "Content-Type": "application/json", // Set the content type to JSON
+          "Content-Type": "application/json",
         },
       }
     );
 
-    // Step 4: Handle the response
     console.log("POST response: ", postResponse.data);
-
-    // Resolve with the relevant information (token or any other response data)
+    const ws = createWebSocketConnection();
     return {
-      token: postResponse.data.timeStampTokenB64, // Assuming the token is returned in this field
+      token: postResponse.data.timeStampTokenB64,
     };
   } catch (err) {
     console.error("Error in attachSignature:", err);
-    throw new Error("Failed to attach signature");
+    throw new Error(err.message);
   }
 };
 
